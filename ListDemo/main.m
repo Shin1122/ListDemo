2572113f17d2361bc2ea662842550b74bf752c31
Shin1122 <big_shin@126.com>
2018-05-12 22:13:18 +0800

    Initial Commit

---
+++ ListDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ListDemo
+//
+//  Created by Lakeside on 2018/5/12.
+//  Copyright © 2018年 Shin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
